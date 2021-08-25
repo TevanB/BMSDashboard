dd04ce8c886999c36fb83943ae2e073279070afc
TevanB <tevan.b@gmail.com>
2021-08-25 19:17:34 -0400

    individually adding two files after npm run dev local

--- resources/js/components/Orders.vue
+++ resources/js/components/Orders.vue
@@ -6,7 +6,7 @@
       <div class="col-md-12">
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title">Orders</h3>
+            <h3 class="card-title">Order List</h3>
             <div class="card-tools">
             <button class="btn btn-success" @click="newModal" v-if="$gate.isAdmin()">Add New
             <i class="fas fa-cart-plus"></i>
